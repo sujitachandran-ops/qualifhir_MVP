a52d855384631f8b94c90536e25ded6b9fc25ba5
CliffordBenjaminRaj <35187738+CliffordBenjaminRaj@users.noreply.github.com>
2025-12-18 15:44:14 +0530

    Create loinc_basic_api.py

---
+++ src/loinc_api/loinc_basic_api.py
@@ -0,0 +1,27 @@
+import json
+import requests
+
+url = "https://loinc.regenstrief.org/searchapi/loincs?query=tumor&rows=100&offset=100&sortorder=loinc_num"
+username = ""
+password = ""
+
+try:
+    response = requests.get(
+        url,
+        auth=(username, password),
+        timeout=10
+    )
+    # response.raise_for_status()
+
+    data = response.json()  # convert response to Python object
+
+    # Write output to JSON file
+    with open(r".\output.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+    print("JSON file created successfully: output.json")
+
+except requests.exceptions.RequestException as e:
+    print("API request failed:", e)
+except json.JSONDecodeError:
+    print("Response is not valid JSON")
